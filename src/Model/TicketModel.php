22c5599b251ced4edbb4129ac0b5503d60e6f280
Budi Arsana <gusdecool@gmail.com>
2016-11-15 12:10:40 +0800

    add ticket status in constants

--- src/Model/TicketModel.php
+++ src/Model/TicketModel.php
@@ -19,6 +19,28 @@ class TicketModel
 {
 
     #----------------------------------------------------------------------------------------------
+    # Constants
+    #----------------------------------------------------------------------------------------------
+
+    const SOURCE_EMAIL = 1;
+    const SOURCE_PORTAL = 2;
+    const SOURCE_PHONE = 3;
+    const SOURCE_CHAT = 7;
+    const SOURCE_MOBIHELP = 8;
+    const SOURCE_FEEDBACK_WIDGET = 9;
+    const SOURCE_OUTBOUND_EMAIL = 10;
+
+    const STATUS_OPEN = 2;
+    const STATUS_PENDING = 3;
+    const STATUS_RESOLVED = 4;
+    const STATUS_CLOSED = 5;
+
+    const PRIORITY_LOW = 1;
+    const PRIORITY_MEDIUM = 2;
+    const PRIORITY_HIGH = 3;
+    const PRIORITY_URGENT = 4;
+
+    #----------------------------------------------------------------------------------------------
     # Properties
     #----------------------------------------------------------------------------------------------
 
